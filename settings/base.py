d66fc9551ebf79a315a49b927a50d195156a8231
pabdelhay <pabdelhay@inoa.com.br>
2013-08-12 19:43:51 -0300

    Settings

--- settings/base.py
+++ settings/base.py
@@ -16,6 +16,10 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
+# DATABASES
+# Must be redefined in local.py and production.py
+DATABASES = {}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
