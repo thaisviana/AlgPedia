dd43480a2f0741d95bb307e60d2d0e1e4d925901
Thais <thaisnviana@gmail.com>
2013-11-04 18:03:44 -0200

    reputation stars

--- algorithm/static/js/algpedia.js
+++ algorithm/static/js/algpedia.js
@@ -1,6 +1,33 @@
 $(function() {	
-	prettyPrint()
+	prettyPrint();
 	
+	$rep = $('.impl-reputation').data('reputation_value');
+	if($rep != null){$rep = $rep.substring(1, 5);}
+	$('.impl-reputation').html('Reputation : <img src="/algorithm/static/images/glyphicons/glyphicons_048_dislikes.png" alt="" />');
+	//var star_0 = "<img src='/algorithm/static/images/glyphicons/glyphicons_048_dislikes.png' alt='"+$rep+"'  width='22' height='22'/>";
+	//var star_1 = "<img src='/algorithm/static/images/glyphicons/glyphicons_049_star.png' alt='"+$rep+"' width='22' height='22' />";
+	var star_0 = "";
+	var star_1 = "<img src='/algorithm/static/images/glyphicons/star.png' alt='"+$rep+"' width='22' height='22'/>";
+	
+	if($rep == 0.0){
+		$('.impl-reputation').html('Reputation : 0.0 '+ star_0 + star_0 + star_0 + star_0+ star_0);
+	}else if($rep <= 0.2){
+		$('.impl-reputation').html('Reputation : '+ star_1 + star_0 + star_0 + star_0+ star_0);
+	}else if(0.2 <= $rep < 0.4){
+		$('.impl-reputation').html('Reputation : '+ star_1 + star_1 + star_0 + star_0+ star_0);
+	}else if(0.4 <= $rep < 0.6){
+		$('.impl-reputation').html('Reputation : '+star_1 + star_1 + star_1 + star_0+ star_0);
+	}else if(0.6 <= $rep < 0.8){
+		$('.impl-reputation').html('Reputation : '+ star_1 + star_1 + star_1 + star_1+ star_0);
+	}else if(0.8 <= $rep < 1.0){
+		$('.impl-reputation').html('Reputation : '+ star_1 + star_1 + star_1 + star_1+ star_1);
+	}
+	$(".impl-reputation").mouseover(function () {
+		return overlib(""+$rep+"", ABOVE);
+	});
+	$(".impl-reputation").mouseout(function () {
+		return nd();
+	});
 	tinyMCE.init({
 		// General options
 		mode : "textareas",
@@ -19,12 +46,6 @@ $(function() {
 		
 	});	
 		
-		
-	//$("#add_implementation").css( "padding-left", "+=900" );
-	
-	//$("#add_evaluation").css( "padding-left", "+=990" );
-		
-	//$("#add_evaluation").click(function(){
 	
 	$("#add_implementation").click(function(){
 		if($('#logged').val()== 'false'){
@@ -35,8 +56,10 @@ $(function() {
 			window.location = "/add/alg/id/"+alg_id;
 		}
 	});
+	
 	$('.textarea').wysihtml5();
-	//$("#add_algorithm").css( "padding-left", "+=800" );
+	
+	
 	$("#add_algorithm").click(function() {
 		event.preventDefault();
 		if($('#logged').val()== 'false'){
