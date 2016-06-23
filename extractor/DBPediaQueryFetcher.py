3b33cc26126e3b4ee8ed6e962b2c0e28f3199a90
csmartins <csmartins.ufrj@gmail.com>
2016-06-23 00:55:39 -0300

    Corrigindo main para inserir dados extraidos no banco

--- extractor/DBPediaQueryFetcher.py
+++ extractor/DBPediaQueryFetcher.py
@@ -9,13 +9,13 @@ import urllib2
 from FileWriters import HTMLWriter, CSVWriter
 
 class QueryFetcher:
-    
-    def __init__(self, fetch_format, temp_folder=os.path.abspath('./extractor/temp')):
-        
+
+    def __init__(self, fetch_format, temp_folder=os.path.abspath('./temp')):
+
 		self.n_fetch = 0
 		self.temp_folder = temp_folder
 		self.base_url = 'http://dbpedia.org/sparql'
-            
+
 		self.get_params = {
 			  'query' : '',
 			  'default-graph-uri' : 'http://dbpedia.org',
@@ -23,17 +23,17 @@ class QueryFetcher:
 			  'timeout' : 0,
 			  'debug' : 'on'
 		}
-		
+
 		# Creating the appropriate type of writer
 		if(fetch_format.upper() == 'HTML'):
 			self.file_writer = HTMLWriter(self.temp_folder)
 		elif(fetch_format.upper() == 'CSV'):
 			self.file_writer = CSVWriter(self.temp_folder)
-            
+
 	# fetch the resource and delegate the writing of the file to the
 	# appropriate instance of FileWriter
     def fetchResult(self, query_string):
-        
+
 		self.get_params['query'] = query_string
 
 		query_url = self.base_url + '?' + urllib.urlencode(self.get_params)
@@ -50,10 +50,8 @@ class QueryFetcher:
 		self.n_fetch += 1;
 
 		return file_path
-    
+
     # change the output path of the fetched resource
     def changeOutputFolder(self, new_folder_path):
         self.temp_folder = new_folder_path
         self.file_writer.updateOutputFolderPath(new_folder_path)
-        
-        
--- extractor/Main.py
+++ extractor/Main.py
@@ -7,81 +7,106 @@ Created on 20/02/2013
 from DBPediaQueryFetcher import QueryFetcher
 from FileWriters import TXTWriter
 import os
+from django.conf import settings
+settings.configure(DEBUG=True)
 
-from algorithm.models import Classification
-
-
+#from extractor.models import Classification
+import MySQLdb
+import random
 
 class ColumnExtractor:
 	def __init__(self, file_name):
 		self.file_name = file_name
-		
+
 	def extractColumn(self, column_index):
 		F_H = open(self.file_name, 'r')
-		
+
 		lines = F_H.readlines()
-		
+
 		F_H.close()
-		
+
 		column = map(lambda x: x.split(',')[column_index], lines)
-		
+
 		return column
 
 # http://live.dbpedia.org/page/Category:Algorithms -> pegar categorias dessa pagina e procurar infoboxes de codigo na wikipedia
 
 def doMain():
-        
+
 	# if no second parameter is passed then the QueryFetcher assumes that there exists ./temp
 	query_fetcher = QueryFetcher('csv')
 
 	dbpedia_master_query = '''select * where{
-	?classification skos:broader <http://dbpedia.org/resource/Category:Algorithms>.
-	?algorithm dcterms:subject ?classification.
-	?algorithm foaf:isPrimaryTopicOf ?wikipedia
-	}'''
+			?classification skos:broader <http://dbpedia.org/resource/Category:Algorithms>.
+			?algorithm foaf:isPrimaryTopicOf ?wikipedia
+			}'''
 
-	filename = query_fetcher.fetchResult(dbpedia_master_query)
-	
-	
+	print "Calling QueryFetcher..."
+	#filename = query_fetcher.fetchResult(dbpedia_master_query)
+	filename = "temp/dbpedia_fetch_0.csv"
+
+	print "Finished fetching results..."
 	# always 0-based, baby
 	insertClassifications(filename, 0)
 
-# returns a list of beautiful names.	
+# returns a list of beautiful names.
 # each name only appears once in this list.
-def extractNames(classif_list):
+def extractClassificationNames(classif_list):
 
 		beautiful_names = dict()
-		
-		names = map(lambda x: x.split(':')[-1], classif_list)
+
+		names = map(lambda x: x.split(':')[2], classif_list)
 		names = map(lambda x: x.replace('_', ' ').title(), names)
-		
+
 		for i in range(0, len(names)):
 			beautiful_names[names[i]] = classif_list[i] if names[i] not in beautiful_names else None
-		
+
 		#for name in names:
-		#	beautiful_names[name] = '' if name not in beautiful_names else None		
-			
+		#	beautiful_names[name] = '' if name not in beautiful_names else None
+
 		return beautiful_names
-	
+
+def extractURI(line):
+	uri = line.split(';')[-1]
+	return uri
+
+def extractClassification(line):
+	classification = line.split(':')[2]
+	classification = classification.replace(';http', '')
+	classification = classification.replace('_', ' ').title()
+	return classification
+
 def insertClassifications(filename, col_number):
 
 	print filename
-	
+	print "Preparing extrated names for insertion..."
+
 	col_extractor = ColumnExtractor(filename)
-	
+
 	col_classification = col_extractor.extractColumn(col_number)
-	
-	classif_names = extractNames(col_classification)
-		
-	txt_Writer = TXTWriter('./')
-	
-	file_name = txt_Writer.writeDictKeysToFile(classif_names, 'classif')
-	
-	for key, val in classif_names.iteritems():
-		aux_classification = Classification(name=key, uri=val)
-		aux_classification.save()
-	
-	
+
+	print "Connecting to database..."
+	db = MySQLdb.connect("localhost","root","123mudar","AlgPedia" )
+	cursor = db.cursor()
+
+	id = 1
+	for line in col_classification:
+		classification = extractClassification(line)
+		uri = extractURI(line)
+		print "Inserting ", classification, uri
+		try:
+			query = "INSERT INTO algorithm_classification VALUES (%s,%s,%s)"
+			print "Executing: ", query
+			cursor.execute(query,(id,classification,uri))
+			db.commit()
+			id = id + 1
+		except MySQLdb.Error as error:
+			print "Error: {}".format(error)
+			#print "Insert failed, rollback"
+			db.rollback()
+	db.close()
+
+
 
 if __name__ == '__main__':
     doMain();
