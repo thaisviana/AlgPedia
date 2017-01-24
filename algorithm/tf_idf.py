ada6233e1c6225944320246d0ca580883e36263a
Fernando Gouvea <fernandosrg1@gmail.com>
2017-01-23 22:03:01 -0200

    Adicionando script para testar os parâmetros do tf-idf/SVD.

--- algorithm/tf_idf.py
+++ algorithm/tf_idf.py
@@ -6,51 +6,57 @@ import codecs
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-path_of_file = os.path.dirname(os.path.realpath(__file__))
+SVD_COMPONENTS = 100
 
-path_of_extractions = path_of_file + "/../extractor/temp/algorithms-txt"
-path_of_output = path_of_file + "/tfidf"
+def main():
+  path_of_file = os.path.dirname(os.path.realpath(__file__))
 
-document_list = []
+  path_of_extractions = path_of_file + "/../extractor/temp/algorithms-txt"
+  path_of_output = path_of_file + "/tfidf"
 
-svd = TruncatedSVD(n_components=100, random_state=17)
-tf_idf_matrix = None
+  document_list = []
 
-print "Preparando corpus..."
-corpus = []
-for document in os.listdir(path_of_extractions):
-  document_list.append(document[:-4])
-  corpus.append("{}/{}".format(path_of_extractions, document))
+  svd = TruncatedSVD(n_components=SVD_COMPONENTS, random_state=17)
+  tf_idf_matrix = None
 
-tf_idf_vectorizer = TfidfVectorizer(input='filename')
+  print "Preparando corpus..."
+  corpus = []
+  for document in os.listdir(path_of_extractions):
+    document_list.append(document[:-4])
+    corpus.append("{}/{}".format(path_of_extractions, document))
 
-print "Montando matriz tf-idf..."
-tf_idf_matrix = tf_idf_vectorizer.fit_transform(corpus)
+  tf_idf_vectorizer = TfidfVectorizer(input='filename')
 
-print "Transformando por SVD..."
-tf_idf_transformed = svd.fit_transform(tf_idf_matrix)
+  print "Montando matriz tf-idf..."
+  tf_idf_matrix = tf_idf_vectorizer.fit_transform(corpus)
 
-if not (os.path.isdir(path_of_output)):
-    os.mkdir(path_of_output)
+  print "Transformando por SVD..."
+  tf_idf_transformed = svd.fit_transform(tf_idf_matrix)
 
-print "Salvando artefatos..."
+  if not (os.path.isdir(path_of_output)):
+      os.mkdir(path_of_output)
 
-documents_file_path = "{}/documents".format(path_of_output)
-documents_file = open(documents_file_path, 'w')
-pickle.dump(document_list, documents_file, pickle.HIGHEST_PROTOCOL)
-documents_file.close()
+  print "Salvando artefatos..."
 
-terms_file_path = "{}/terms".format(path_of_output)
-terms_file = open(terms_file_path, 'w')
-pickle.dump(tf_idf_vectorizer.vocabulary_, terms_file, pickle.HIGHEST_PROTOCOL)
-terms_file.close()
+  documents_file_path = "{}/documents".format(path_of_output)
+  documents_file = open(documents_file_path, 'w')
+  pickle.dump(document_list, documents_file, pickle.HIGHEST_PROTOCOL)
+  documents_file.close()
 
-matrix_file_path = "{}/matrix".format(path_of_output)
-matrix_file = open(matrix_file_path, 'w')
-pickle.dump(tf_idf_transformed, matrix_file, pickle.HIGHEST_PROTOCOL)
-matrix_file.close()
+  terms_file_path = "{}/terms".format(path_of_output)
+  terms_file = open(terms_file_path, 'w')
+  pickle.dump(tf_idf_vectorizer.vocabulary_, terms_file, pickle.HIGHEST_PROTOCOL)
+  terms_file.close()
 
-svd_components_file_path = "{}/svd_components".format(path_of_output)
-svd_components_file = open(svd_components_file_path, 'w', pickle.HIGHEST_PROTOCOL)
-pickle.dump(svd.components_, svd_components_file)
-svd_components_file.close()
+  matrix_file_path = "{}/matrix".format(path_of_output)
+  matrix_file = open(matrix_file_path, 'w')
+  pickle.dump(tf_idf_transformed, matrix_file, pickle.HIGHEST_PROTOCOL)
+  matrix_file.close()
+
+  svd_components_file_path = "{}/svd_components".format(path_of_output)
+  svd_components_file = open(svd_components_file_path, 'w', pickle.HIGHEST_PROTOCOL)
+  pickle.dump(svd.components_, svd_components_file)
+  svd_components_file.close()
+
+if __name__ == "__main__":
+  main()
